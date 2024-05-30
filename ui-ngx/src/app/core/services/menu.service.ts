4d8a5d00620655a69035c3b932d34b987596e747
zhuxianjun <zhuxianjun>
2024-05-30 08:44:59 +0800

    修改测试

--- ui-ngx/src/app/core/services/menu.service.ts
+++ ui-ngx/src/app/core/services/menu.service.ts
@@ -535,13 +535,13 @@ export class MenuService {
               }
             ]
           },
-          {
-            id: 'images',
-            name: 'image.gallery',
-            type: 'link',
-            path: '/resources/images',
-            icon: 'filter'
-          },
+          // {
+          //   id: 'images',
+          //   name: '图库',
+          //   type: 'link',
+          //   path: '/resources/images',
+          //   icon: 'filter'
+          // },
           {
             id: 'resources_library',
             name: 'resource.resources-library',
